493321a55ccea7c90cfdf1e7647be654fc6802cd
Andrei Khabarov <akhabarov@qase.io>
2023-04-13 22:29:52 +0400

    Prettier + linting

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
 import './App.css'
 
 function App() {
-
-  return (
-    <div className="App">
-      Qase-report
-    </div>
-  )
+  return <div className="App">Qase-report</div>
 }
 
 export default App
