6cdac44dbe46e8bca32e59e40b9cec8d932e7764
Dmitrii Gridnev <dgridnev@qase.io>
2026-02-09 19:11:18 +0300

    feat(02-01): integrate dashboard into main layout
    - Imported Dashboard component into MainLayout
    - Replaced placeholder text with Dashboard component
    - Kept LoadReportButton and sidebar controls intact
    - Dashboard displays below LoadReportButton

--- src/layout/MainLayout/index.tsx
+++ src/layout/MainLayout/index.tsx
@@ -3,6 +3,7 @@ import { Button, Grid } from '@mui/material'
 import { useRootStore } from '../../store'
 import { Sidebar } from '../../components/Sidebar'
 import { LoadReportButton } from '../../components/LoadReportButton'
+import { Dashboard } from '../../components/Dashboard'
 
 export const MainLayout = observer(() => {
   const { isDockOpen, closeDock, openDock } = useRootStore()
@@ -18,10 +19,9 @@ export const MainLayout = observer(() => {
         <Button type={'button'} onClick={openDock}>
           Open sidebar
         </Button>
+        <Dashboard />
       </Grid>
-      <Grid item xs={2}>
-        Hello from smaller container
-      </Grid>
+      <Grid item xs={2}></Grid>
       <Sidebar isOpen={isDockOpen} onClose={closeDock}>
         Hello from sidebar
       </Sidebar>
