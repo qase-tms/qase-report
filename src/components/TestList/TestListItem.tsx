0a383751cd3c5cbef162c129b759dd9c35c86f8e
Dmitrii Gridnev <dgridnev@qase.io>
2026-02-10 14:20:55 +0300

    feat(10-02): integrate StabilityBadge into TestListItem
    - Replace memo() with observer() for MobX reactivity
    - Add useRootStore hook to access analyticsStore
    - Call getFlakinessScore for tests with signatures
    - Display StabilityBadge on test list items with history data

--- src/components/TestList/TestListItem.tsx
+++ src/components/TestList/TestListItem.tsx
@@ -1,21 +1,29 @@
-import { memo } from 'react'
+import { observer } from 'mobx-react-lite'
 import { ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material'
 import type { QaseTestResult } from '../../schemas/QaseTestResult.schema'
 import { getStatusIcon } from './statusIcon'
+import { useRootStore } from '../../store'
+import { StabilityBadge } from './StabilityBadge'
 
 interface TestListItemProps {
   test: QaseTestResult
   onSelect: (id: string) => void
 }
 
-export const TestListItem = memo(({ test, onSelect }: TestListItemProps) => {
+export const TestListItem = observer(({ test, onSelect }: TestListItemProps) => {
   const handleClick = () => onSelect(test.id)
+  const { analyticsStore } = useRootStore()
 
   // Format duration: if > 1000ms show as seconds
   const duration = test.execution.duration
   const durationText = duration >= 1000
     ? `${(duration / 1000).toFixed(1)}s`
     : `${duration}ms`
+
+  // Get flakiness result if test has signature
+  const flakinessResult = test.signature
+    ? analyticsStore.getFlakinessScore(test.signature)
+    : null
 
   return (
     <ListItem disablePadding>
@@ -28,6 +36,7 @@ export const TestListItem = memo(({ test, onSelect }: TestListItemProps) => {
           secondary={durationText}
           primaryTypographyProps={{ noWrap: true }}
         />
+        {flakinessResult && <StabilityBadge result={flakinessResult} />}
       </ListItemButton>
     </ListItem>
   )
